3cf79abb3125536a20967d6064fa7eccf6275ca7
Yunus <yunuskussiev2007@mail.ru>
2023-03-25 03:21:18 +0300

    first commit

---
+++ src/data/productColors.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Blue',
+    hex: '#73B6EA'
+  },
+  {
+    id: 1,
+    name: 'Yellow',
+    hex: '#FFBE15'
+  },
+  {
+    id: 1,
+    name: 'Gray',
+    hex: '#939393'
+  },
+  {
+    id: 1,
+    name: 'Green',
+    hex: '#8BE000'
+  },
+  {
+    id: 1,
+    name: 'Orange',
+    hex: '#FF6B00'
+  },
+  {
+    id: 1,
+    name: 'White',
+    hex: '#FFF'
+  },
+  {
+    id: 1,
+    name: 'Black',
+    hex: '#000'
+  },
+]
